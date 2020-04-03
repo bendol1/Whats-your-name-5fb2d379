353c163c8b94d05709b8853b6a8aa636ff5ec897
Ben <2088222@talnet.nl>
2020-04-03 13:39:59 +0200

    Uitwerking van de exercise

---
+++ hello.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Wat is jouw voornaam? ";
+
+$voornaam = readline();
+
+echo "Wat is jouw achternaam? ";
+
+$achternaam = readline();
+
+echo "Jouw naam is $voornaam $achternaam ";
+
+
+
+
+
+?>
